63475ad51b0970dfca58ecc9f270e04ff6c55be7
procamora <pablojoserocamora@gmail.com>
2020-11-01 13:24:50 +0100

    update CI

--- iaac/variables.sh
+++ iaac/variables.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034  # Unused variables, file load for scripts
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
 
 declare -r MY_PATH="/root/prometeo"
 
