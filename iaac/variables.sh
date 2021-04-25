58180cb1b8005906f873368e0bf35bbc9ec261d9
procamora <pablojose.rocamora.next@bbva.com>
2021-04-25 16:56:17 +0200

    update

--- iaac/variables.sh
+++ iaac/variables.sh
@@ -82,7 +82,9 @@ declare -r IP_IDS_HEALTH="10.30.0.3"
 declare -r VMID_LAB_HEALTH="225"
 declare -r IP_LAB_HEALTH="172.0.0.254"
 
-# VMID LAN SERVER
+###################
+# VMID LAN SERVER #
+###################
 declare -r VLAN_LAN="10"
 declare -r MASK_LAN="24"
 declare -r NETMASK_LAN="255.255.255.0"
@@ -90,25 +92,34 @@ declare -r GATEWAY_LAN="10.10.0.1"
 
 declare -r VMID_LAN_ASTERISK="321"
 declare -r IP_LAN_ASTERISK="10.10.0.21"
+
 declare -r VMID_LAN_LDAP="322"
 declare -r IP_LAN_LDAP="10.10.0.22"
+
 declare -r VMID_LAN_ELK="323"
 declare -r IP_LAN_ELK="10.10.0.23"
+
 declare -r VMID_LAN_SPLUNK="324"
 declare -r IP_LAN_SPLUNK="10.10.0.24"
+
 declare -r VMID_LAN_WINSERVER="325"
 declare -r IP_LAN_WINSERVER="10.10.0.25"
+
 declare -r VMID_LAN_OAUTH="326"
 declare -r IP_LAN_OAUTH="10.10.0.26"
+
 declare -r VMID_LAN_OPENID="327"
 declare -r IP_LAN_OPENID="10.10.0.27"
+
 declare -r VMID_LAN_SQLSERVER="328"
 declare -r IP_LAN_SQLSERVER="10.10.0.28"
+
 declare -r VMID_LAN_ANSIBLE="329"
 declare -r IP_LAN_ANSIBLE="10.10.0.29"
 
-
-# VMID LAN PCS
+################
+# VMID LAN PCS #
+################
 declare -r VLAN_PC="1"
 declare -r MASK_PC="24"
 declare -r NETMASK_PC="255.255.255.0"
@@ -116,55 +127,126 @@ declare -r GATEWAY_PC="10.10.11.1"
 
 declare -r VMID_PC_WIN10="421"
 declare -r IP_PC_WIN10="10.10.11.82"
+
 declare -r VMID_PC_WIN7="422"
 declare -r IP_PC_WIN7="10.10.11.83"
+
 declare -r VMID_PC_WINXP="423"
 declare -r IP_PC_WINXP="10.10.11.84"
+
 declare -r VMID_PC_DEBIAN="424"
 declare -r IP_PC_DEBIAN="10.10.11.85"
+
 declare -r VMID_PC_MACOS="425"
 declare -r IP_PC_MACOS="10.10.11.86"
 
-# VMID DMZ SERVERS
+####################
+# VMID DMZ SERVERS #
+####################
 declare -r VLAN_DMZ="20"
 declare -r MASK_DMZ="24"
 declare -r NETMASK_DMZ="255.255.255.0"
 declare -r GATEWAY_DMZ="10.20.0.1"
 
 declare -r VMID_DMZ_MONGO="521"
+declare -r PORT_DMZ_MONGO="27017"
+declare -r PROTCOL_DMZ_MONGO="http"
+declare -r PROXY_DMZ_MONGO="false"
 declare -r IP_DMZ_MONGO="10.20.0.21"
+
 declare -r VMID_DMZ_MARIADB="522"
+declare -r PORT_DMZ_MARIADB="3306"
+declare -r PROTOCOL_DMZ_MARIADB="http"
+declare -r PROXY_DMZ_MARIADB="false"
 declare -r IP_DMZ_MARIADB="10.20.0.22"
+
 declare -r VMID_DMZ_VSFTPD="523"
+declare -r PORT_DMZ_VSFTPD="21"
+declare -r PROTOCOL_DMZ_VSFTPD="http"
+declare -r PROXY_DMZ_VSFTPD="false"
 declare -r IP_DMZ_VSFTPD="10.20.0.23"
+
 declare -r VMID_DMZ_DOJO="524"
+declare -r PORT_DMZ_DOJO="80"
+declare -r PROTOCOL_DMZ_DOJO="http"
+declare -r PROXY_DMZ_DOJO="false"
 declare -r IP_DMZ_DOJO="10.20.0.24"
+
 declare -r VMID_DMZ_APACHE="525" # VARIOS CMS MENOS CONOCIDOS Moodle vBulletin CmsBundle Silverstripe
+declare -r PORT_DMZ_APACHE="80"
+declare -r PROTOCOL_DMZ_APACHE="http"
+declare -r PROXY_DMZ_APACHE="false"
 declare -r IP_DMZ_APACHE="10.20.0.25"
+
 declare -r VMID_DMZ_NAGIOS="526"
+declare -r PORT_DMZ_NAGIOS="80"
+declare -r PROTOCOL_DMZ_NAGIOS="http"
+declare -r PROXY_DMZ_NAGIOS="false"
 declare -r IP_DMZ_NAGIOS="10.20.0.26"
+
 declare -r VMID_DMZ_MUNA="527"
+declare -r PORT_DMZ_MUNA="80"
+declare -r PROTOCOL_DMZ_MUNA="80"
+declare -r PROXY_DMZ_MUNA="false"
 declare -r IP_DMZ_MUNA="10.20.0.27"
+
 declare -r VMID_DMZ_OWNCLOUD="528"
+declare -r PORT_DMZ_OWNCLOUD="false"
+declare -r PROTOCOL_DMZ_OWNCLOUD="http"
+declare -r PROXY_DMZ_OWNCLOUD="80"
 declare -r IP_DMZ_OWNCLOUD="10.20.0.28"
+
 declare -r VMID_DMZ_JOOMLA="529"
+declare -r PORT_DMZ_JOOMLA="80"
+declare -r PROTOCOL_DMZ_JOOMLA="http"
+declare -r PROXY_DMZ_JOOMLA="false"
 declare -r IP_DMZ_JOOMLA="10.20.0.29"
+
 declare -r VMID_DMZ_PRESTASHOP="530"
+declare -r PORT_DMZ_PRESTASHOP="80"
+declare -r PROTOCOL_DMZ_PRESTASHOP="http"
+declare -r PROXY_DMZ_PRESTASHOP="false"
 declare -r IP_DMZ_PRESTASHOP="10.20.0.30"
+
 declare -r VMID_DMZ_DRUPAL="531"
+declare -r PORT_DMZ_DRUPAL="80"
+declare -r PROTOCOL_DMZ_DRUPAL="http"
+declare -r PROXY_DMZ_DRUPAL="false"
 declare -r IP_DMZ_DRUPAL="10.20.0.31"
+
 declare -r VMID_DMZ_WORDPRESS="532"
+declare -r PORT_DMZ_WORDPRESS="80"
+declare -r PROTOCOL_DMZ_WORDPRESS="http"
+declare -r PROXY_DMZ_WORDPRESS="false"
 declare -r IP_DMZ_WORDPRESS="10.20.0.32"
+
 declare -r VMID_DMZ_RADIUS="533"
+declare -r PORT_DMZ_RADIUS="1812"
+declare -r PROTOCOL_DMZ_RADIUS="http"
+declare -r PROXY_DMZ_RADIUS="false"
 declare -r IP_DMZ_RADIUS="10.20.0.33"
+
 declare -r VMID_DMZ_DNS="534"
+declare -r PORT_DMZ_DNS="53"
+declare -r PROTOCOL_DMZ_DNS="http"
+declare -r PROXY_DMZ_DNS="false"
 declare -r IP_DMZ_DNS="10.20.0.34"
+
 declare -r VMID_DMZ_HONEYPOT="535"
+declare -r PORT_DMZ_HONEYPOT="22"
+declare -r PROTOCOL_DMZ_HONEYPOT="http"
+declare -r PROXY_DMZ_HONEYPOT="false"
 declare -r IP_DMZ_HONEYPOT="10.20.0.35"
+
 declare -r VMID_DMZ_MAIL="536"
+declare -r PORT_DMZ_MAIL="25"
+declare -r PROTOCOL_DMZ_MAIL="http"
+declare -r PROXY_DMZ_MAIL="false"
 declare -r IP_DMZ_MAIL="10.20.0.36"
 
-# VMID SURICATA
+#################
+# VMID SURICATA #
+#################
 declare -r VLAN_IDS="30"
 declare -r MASK_IDS="29"
 declare -r NETMASK_IDS="255.255.255.248"
@@ -173,7 +255,9 @@ declare -r GATEWAY_IDS="10.30.0.1"
 declare -r VMID_IDS_SURICATA="621"
 declare -r IP_IDS_SURICATA="10.30.0.2"
 
-# VMID LABORARTIY
+###################
+# VMID LABORARTIY #
+###################
 declare -r VLAN_LAB="1"
 declare -r MASK_LAB="24"
 declare -r NETMASK_LAB="255.255.255.0"
