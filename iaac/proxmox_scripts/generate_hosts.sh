30fa354d154a2b94b2bda587ddfc832fce08e528
procamora <pablojose.rocamora.next@bbva.com>
2021-04-25 16:55:14 +0200

    update

--- iaac/proxmox_scripts/generate_hosts.sh
+++ iaac/proxmox_scripts/generate_hosts.sh
@@ -44,14 +44,18 @@ function generate_inventary() {
     #inventory_yml+="  hosts:\n"
     for my_ip in "${my_ips_dmz[@]}"; do
         host=$(echo "$my_ip" | awk -F "_" '{print $3}' | awk -F "=" '{print tolower($1)}')
+        proxy=$(grep -iE "PROXY_(DMZ|IDS|LAN)_$host" "$vars_files" | awk -F " " '{print $2}' | tr -d '"')
+        protocol=$(grep -iE "PROTOCOL_(DMZ|IDS|LAN)_$host" "$vars_files" | awk -F " " '{print $2}' | tr -d '"')
+        port=$(grep -iE "PORT_(DMZ|IDS|LAN)_$host" "$vars_files" | awk -F " " '{print $2}' | tr -d '"')
         inventory_yml+="  $host:\n"
         inventory_yml+="    service: $host\n"
         inventory_yml+="    name: $host.$DOMAIN\n"
-        inventory_yml+="    port: $host.$DOMAIN\n"
-        inventory_yml+="    protocol: $host.$DOMAIN\n"
+        inventory_yml+="    port: $port\n"
+        inventory_yml+="    protocol: $protocol\n"
         inventory_yml+="    name_rp: ${host}.rp.${DOMAIN}\n"
         inventory_yml+="    ip: $my_ip\n"
-        inventory_yml+="    rp: $host.$DOMAIN\n"
+        inventory_yml+="    rp: $proxy\n"
+        inventory_yml+="  \n"
     done
     inventory_yml+="  vars:\n"
     inventory_yml+="    ansible_user: root\n"
